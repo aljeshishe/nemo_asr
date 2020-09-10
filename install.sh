6f6d5020035d695190e40415b3b2ae35ad02a92b
Alexey Grachev <ax66@bk.ru>
2020-09-10 15:22:49 +0300

    added intsalling nemo from my repo

--- install.sh
+++ install.sh
@@ -4,7 +4,5 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 pip install torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 sudo apt install sox libsndfile1 ffmpeg wget -y
-pip install nemo_toolkit[asr]==0.11.0 unidecode wget frozendict wandb==0.9.7
-# kaldi_io torch-stft==0.1.4 soundfile==0.10.3.post1
-mkdir -p configs
-wget -P configs/ https://raw.githubusercontent.com/NVIDIA/NeMo/master/examples/asr/notebooks/configs/jasper_an4.yaml
+pip install unidecode wget frozendict wandb==0.9.7
+pip install -e git://github.com/aljeshishe/nemo.git@v0.11.0#egg=nemo[asr]
