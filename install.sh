b56c2e81fd44374dc8891fa4d8e833dc3e84082d
Alexey Grachev <ax66@bk.ru>
2020-09-10 16:22:03 +0300

    RuntimeError: Can't call numpy() on Variable that requires grad

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ export DEBIAN_FRONTEND=noninteractive
 pip install torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 sudo apt install sox libsndfile1 ffmpeg wget -y
 pip install unidecode wget frozendict wandb==0.9.7
-pip install -e git://github.com/aljeshishe/nemo.git@v0.11.0#egg=nemo[asr]
+pip install -e git://github.com/aljeshishe/nemo.git@93997a2c#egg=nemo[asr]
